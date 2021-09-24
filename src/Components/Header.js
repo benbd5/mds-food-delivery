e04113f067c66151d224fb7735b11de1730d4649
benbd5 <belzbenjaminp@gmail.com>
2021-09-24 14:35:49 +0200

    add router and navigation

---
+++ src/Components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+
+class Header extends React.Component {
+    render() {
+    return (
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Accueil</Link>
+            </li>
+            <li>
+              <Link to="/restaurants">Restaurants</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ src/Pages/Restau.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../App.css';
+
+class Restau extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Restaurants</h2>
+      </div>
+    );
+  }
+}
+
+export default Restau;
